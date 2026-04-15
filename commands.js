df8232f82cd24ad580f92930812919f0f6354845
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-04-14 23:44:42 -0300

    Estudando o código e fazendo as alterações necessárias

---
+++ commands.js
@@ -0,0 +1,27 @@
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+Jasmin= [
+    "<br>",
+    "Agente:Jasmin",
+    "Nome:Maria Flor",
+    "<br>"
+
+]
+
+banner = [
+//  '<span class="index">Bem vindo Administrador.</span>',
+"██████╗ ███████╗███╗   ███╗██████╗ ",
+"██╔══██╗██╔════╝████╗ ████║██╔══██╗",
+"██║  ██║█████╗  ██╔████╔██║██████╔╝",
+"██║  ██║██╔══╝  ██║╚██╔╝██║██╔═══╝ ",
+"██████╔╝███████╗██║ ╚═╝ ██║██║   ",  
+"╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ",
+"DEPARTAMENTO DE EXTERMÍNIO DE",
+"MANIFESTAÇÕES PARANORMAIS",   
+  '<span class="color2">Bem vindo <i>Administrador</i></span>',
+  "<span class=\"color2\">Digite o nome do Agente ou Ex-Cidadão para saber <i>mais<i></span><span class=\"color2\">.</span>",
+];
